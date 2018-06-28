8d454026b05237b2c33cda108fa888b7e04f6fd3
alexbostock <alex@alexbostock.co.uk>
2018-06-28 22:28:10 +0100

    Implement indent size-detection

---
+++ detect-indent.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+
+// Should print an integer: number of spaces / 0 for tabs
+// In case of error, just guess 4
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		printf("4");
+		return 1;
+	}
+
+	FILE *fp = fopen(argv[1], "r");
+
+	if (fp == NULL) {
+		printf("4");
+		return 1;
+	}
+
+	char line[80];
+	// We will process at most 79 characters of each line
+
+        int line_num = 0;
+        int num_lines_with_tabs = 0;
+
+        unsigned int num_spaces[1000];
+        // We will process at most 1000 lines
+
+        for (int i = 0; i < 1000; i++)
+		num_spaces[i] = 0;
+
+	for (line_num = 0; line_num < 1000 && !feof(fp); line_num++) {
+		fgets(line, 80, fp);
+		for (int i = 0; i < 80; i++) {
+			switch (line[i]) {
+				case '\0':
+				case '\n':
+				        continue;
+
+			        case '\t':
+			                num_spaces[line_num] = -1;
+			                num_lines_with_tabs++;
+			                break;
+
+		                case ' ':
+		                        num_spaces[line_num]++;
+		                        break;
+
+	                        default:
+		                        continue;
+			}
+		}
+	}
+
+	int num_indented_lines = 0;
+        for (int i = 0; i < line_num; i++) {
+	        if (num_spaces[i] != 0) {
+		        num_indented_lines++;
+	        }
+        }
+
+	if (num_lines_with_tabs > num_indented_lines / 2) {
+		printf("0");
+	} else {
+		// Assume the indent size is between 2 and 8
+		// Find the Highest one which fits >50% of lines
+
+		for (int i = 8; i >= 2; i--) {
+			int num_ok = 0;
+			for (int j = 0; j < line_num; j++) {
+				if (num_spaces[j] > 0 && num_spaces[j] % i == 0) {
+					num_ok++;
+				}
+			}
+
+			if (num_ok > num_indented_lines / 2) {
+				printf("%d", i);
+				break;
+			}
+		}
+
+		// Default case, just guess 2
+		printf("2");
+	}
+
+	fclose(fp);
+
+	return 0;
+}
